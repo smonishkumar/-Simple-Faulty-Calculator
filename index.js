3dd95a20db030606f5ff052a7b1ef2d2f19fdb56
S Monish Kumar <monish.github@gmail.com>
2025-02-18 00:28:28 +0530

    Update code

--- index.js
+++ index.js
@@ -9,6 +9,32 @@ This faulty calculator does following:
 - ---> /
 / ---> **
 
-It performs wrong operation 10% of the times
+3.It performs wrong operation 10% of the times
 
 */
+
+let random=Math.random()
+// console.log(random)            // Can be used to check when faulty reading is obtained
+let a = prompt("Enter first number")
+let c = prompt("Enter operation")
+let b = prompt("Enter second number")
+
+let obj={
+    "+":"-",
+    "*":"+",
+    "-":"/",
+    "/":"**"
+}
+
+if (random>0.1) {
+    // Perform correct calculation
+    console.log(`The result is ${a} ${c} ${b}`)   //Use backtick(`) and not singlequote(')
+}
+
+else{
+    // Perform wrong calculation
+    c=obj[c]    
+    console.log("The result is ${a} ${c} ${b}")    
+}
+    
+
